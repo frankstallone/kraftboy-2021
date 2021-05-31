4d5fd108d7abccf2837996c4abe3a93151b52f5e
Frank Stallone III <frank@stallone.dev>
2021-05-31 12:03:56 -0400

    init: 🎉 initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -138,7 +138,7 @@ const IndexPage = () => {
           🎉🎉🎉
         </span>
       </h1>
-      <p style={paragraphStyles}>
+      <p style={paragraphStyles} className="text-xl">
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
