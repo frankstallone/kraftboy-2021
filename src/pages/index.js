25a6b11bb8bae08b7662814e0ab1387306bfdafa
Frank Stallone III <frank@stallone.dev>
2021-06-02 19:08:22 -0400

    feat: ✨ Roughed in PayPal buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,25 @@
-import * as React from "react"
+import * as React from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 // markup
 const IndexPage = () => {
   return (
-    <main className="flex flex-col flex-1">
-      <div className="min-h-screen py-24 px-4 relative overflow-hidden">
-      <title>kraftboy products</title>
-      <h1 className="font-serif">
-        kraftboy products - made in america
-      </h1>
-      <h2 className="font-serif text-9xl font-extrabold tracking-tightest">Compadre</h2>
-      <h3>The Travel Guitar Stabilizer</h3>
-      </div>
-    </main>
-  )
-}
+    <PayPalScriptProvider options={{ "client-id": "test" }}>
+      <main className="flex flex-col flex-1">
+        <div className="min-h-screen py-24 px-4 relative overflow-hidden">
+          <title>kraftboy products</title>
+          <h1 className="font-serif">kraftboy products - made in america</h1>
+          <h2 className="font-serif text-9xl font-extrabold tracking-tightest">
+            Compadre
+          </h2>
+          <h3>The Travel Guitar Stabilizer</h3>
+        </div>
+        <div className="py-24 px-4">
+          <PayPalButtons style={{ layout: "horizontal" }} />
+        </div>
+      </main>
+    </PayPalScriptProvider>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
